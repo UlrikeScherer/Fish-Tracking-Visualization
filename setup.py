35d49398a6b37e869bb9ff0b621b092f55027be4
Luka Stärk <luka.staerk@outlook.com>
2022-05-06 16:26:30 +0200

    fiexd setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ import numpy
 extensions=[
         Extension("methods", ["src/methods.pyx"],
                   include_dirs=[numpy.get_include()],
-                  define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')])
+                  #define_macros=[('NPY_NO_DEPRECATED_API', 'NPY_1_7_API_VERSION')]
+                  )
     ]
 setup(
     ext_modules=cythonize(extensions),
