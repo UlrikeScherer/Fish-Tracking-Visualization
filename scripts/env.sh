658262129ebf17ce8ff0585dcd69a05e8a8c6c2c
Ulrike Scherer <u.k.scherer@gmail.com>
2022-05-06 13:33:54 +0200

    rikes commit

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 # src/utile.py imports the paths of this file and the bash scripts in tex/ to generate the pdfs do the same
-BLOCK="block1" # SPICIFY THE BLOCK TO RUN THE SCRIPT ON (block1, block2,...)
+
+BLOCK="block2" # SPICIFY THE BLOCK TO RUN THE SCRIPT ON (block1, block2,...)
 RECORDINGTIME="060000" # START TIME FOR THE EXPERIMENT
 FEEDINGTIME="140000" # START TIME FOR THE FEEDING SETUP
 STIME=$RECORDINGTIME   
+
 rootserver="/Volumes/data/loopbio_data/FE_(fingerprint_experiment)_SepDec2021"  # On MocOS the path to the root of the data
-root_local="/Volumes/Extreme_SSD" # ".."
+#root_local="/Volumes/Extreme_SSD" # ".."
+root_local="/Users/rike/Desktop" # ".."
 path_recordings="$rootserver/FE_recordings/FE_recordings_$BLOCK/FE_${BLOCK}_recordings_*" # path to the recordings, will allway be on the server
-path="FE_tracks_retracked/FE_tracks_${STIME}_${BLOCK}_retracked"
+path="FE_${BLOCK}_${STIME}_retracks_final"
 #path_original="FE_tracks_original/FE_tracks_${STIME}/FE_tracks_${STIME}_$BLOCK"
 path_csv="$rootserver/$path"
 
 path_csv_local="$root_local/$path"
-POSITION_STR_FRONT="FE_${STIME}_tracks_${BLOCK}_front_retracked"
-POSITION_STR_BACK="FE_${STIME}_tracks_${BLOCK}_back_retracked"
+POSITION_STR_FRONT="FE_${BLOCK}_${STIME}_front_final"
+POSITION_STR_BACK="FE_${BLOCK}_${STIME}_front_final"
 
 path_csv_feeding="$rootserver/FE_tracks_original/FE_tracks_${FEEDINGTIME}/FE_tracks_${FEEDINGTIME}_$BLOCK"
 path_csv_feeding_local="$root_local/FE_tracks_original/FE_tracks_${FEEDINGTIME}/FE_tracks_${FEEDINGTIME}_$BLOCK"
