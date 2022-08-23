b21d1662f4ff90f3f492dedd71205ac2d0535904
Luka <luka.staerk@campus.tu-berlin.de>
2022-08-23 16:47:22 +0200

    update hpc berman

--- src/data_factory/processing.py
+++ src/data_factory/processing.py
@@ -1,4 +1,4 @@
-import os
+import os, glob
 import pandas as pd
 from src.methods import turning_directions, distance_to_wall_chunk, calc_steps
 from src.metrics.tank_area_config import get_area_functions
@@ -30,32 +30,42 @@ def transform_to_traces_high_dim(data, filter_index, area_tuple):
     X = np.array((np.arange(1,L-1),xy_steps[:-1,0],xy_steps[:-1,1], t_a, data[1:-1,0], data[1:-1,1])).T 
     return X[~f3], new_area
 
-def compute_projections(fish_key, day, area_tuple, write_file=False):
+def compute_projections(fish_key, day, area_tuple):
     cam, pos = fish_key.split("_")
     is_back = pos==BACK
-    file_path = f"{projectPath}/Projections/{BLOCK}_{cam}_{pos}_{day}.npy"
-    if os.path.exists(file_path):
-        return np.load(file_path)
-    data = pd.concat(csv_of_the_day(cam,day, is_back=is_back)[1])
+    data_in_batches = csv_of_the_day(cam,day, is_back=is_back)[1]
+    if len(data_in_batches)==0:
+        print("%s for day %s is empty! "%(fish_key,day))
+        return None,None
+    data = pd.concat(data_in_batches)
     data_px = data[["xpx", "ypx"]].to_numpy()
     filter_index = all_error_filters(data_px, area_tuple)
     X, new_area = transform_to_traces_high_dim(data_px, filter_index, area_tuple)
-    if write_file:
-        np.save(file_path,X)
     return X, new_area
 
-def compute_all_projections(fish_keys=None):
+def compute_all_projections(fish_keys=None, recompute=False):
     area_f = get_area_functions()
     if fish_keys is None:
         fish_keys = get_camera_pos_keys()
     for fk in fish_keys:
         days = get_days_in_order(camera=fk.split("_")[0], is_back=fk.split("_")[1]==BACK)
         for day in days:
+            filename = projectPath + f'/Projections/{BLOCK}_{fk}_{day}_pcaModes.mat'
+            if not recompute and os.path.exists(filename):
+                continue
             area_tuple = (fk, area_f(fk,day))
-            X, new_area = compute_projections(fk, day, area_tuple, write_file=True)
+            X, new_area = compute_projections(fk, day, area_tuple)
+            if X is None: continue
             print(f"{fk} {fish_keys.index(fk)} {day} {days.index(day)} {X.shape}", flush=True)
-            hdf5storage.write(data={'projections': X[:,1:4], 'positions': X[:,4:], 'area':new_area, 'index':X[:,0]}, path='/', truncate_existing=True,
-                        filename=projectPath + f'/Projections/{BLOCK}_{fk}_{day}_pcaModes.mat',
+            if X.shape[0]<1000:
+                print("Skipe number of datapoints to small")
+                continue
+            hdf5storage.write(data={'projections': X[:,1:4], 
+                                    'positions': X[:,4:], 
+                                    'area':new_area, 
+                                    'index':X[:,0]},
+                              path='/', truncate_existing=True,
+                        filename=filename,
                         store_python_metadata=False, matlab_compatible=True)
             
 
@@ -90,7 +100,29 @@ def subsample_wavelet_and_embeddings(parameters, fish_keys=None):
                           filename=training_data_file + 'training_data.mat', store_python_metadata=False,
                           matlab_compatible=True)
 
+def load_trajectory_data(fk, parameters):
+    data_by_day = []
+    pfile = glob.glob(parameters.projectPath+f'/Projections/{BLOCK}_{fk}_*_pcaModes.mat')
+    for f in pfile: 
+        data = hdf5storage.loadmat(f)
+        data_by_day.append(data)
+    return data_by_day
 
+def load_zVals(fk, parameters):
+    data_by_day = []
+    zValstr = get_zValues_str(parameters)
+    pfile = glob.glob(parameters.projectPath+f'/Projections/{BLOCK}_{fk}_*_pcaModes_{zValstr}.mat')
+    for f in pfile: 
+        data = hdf5storage.loadmat(f)
+        data_by_day.append(data)
+    return data_by_day
+
+def get_zValues_str(parameters):
+    if parameters.method == 'TSNE':
+        zValstr = 'zVals'
+    else:
+        zValstr = 'uVals'
+    return zValstr
 
 
 
