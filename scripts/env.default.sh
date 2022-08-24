00f3a61e5ef9860ca4621b6aee0c9f37da27d930
Luka Stärk <luka.staerk@outlook.com>
2022-08-24 18:26:47 +0200

    update

--- scripts/env.default.sh
+++ scripts/env.default.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-# src/config.py imports the paths of this file and the bash scripts in tex/ to generate the pdfs do the same
+# src/utile.py imports the paths of this file and the bash scripts in tex/ to generate the pdfs do the same
 # --- BLOCK -------
 BLOCK="block1" # SPICIFY THE BLOCK TO RUN THE SCRIPT ON (block1, block2,...)
 # -----------------
-
 RECORDINGTIME="060000" # START TIME FOR THE EXPERIMENT
 FEEDINGTIME="140000" # START TIME FOR THE FEEDING SETUP
 STIME=$RECORDINGTIME   
-
 rootserver="/Volumes/data/loopbio_data/FE_(fingerprint_experiment)_SepDec2021"  # On MocOS the path to the root of the data
-
-root_local="/Volumes/Extreme_SSD" # ".."
 path_recordings="$rootserver/FE_recordings/FE_recordings_$BLOCK/FE_${BLOCK}_recordings_*" # path to the recordings, will allway be on the server
-path="FE_${BLOCK}_${STIME}_retracks_final"
+
+# --- The Path to the local data------------
+root_local="/Volumes/Extreme_SSD" # ".." "/Users/lukastaerk/fish" #
+path="FE_tracks"
+#path_original="FE_tracks_original/FE_tracks_${STIME}/FE_tracks_${STIME}_$BLOCK"
 # --------------------------------------
 
 # TRAJECTORY PATHS
@@ -24,9 +24,8 @@ dir_front=$path_csv_local/$POSITION_STR_FRONT
 dir_back=$path_csv_local/$POSITION_STR_BACK
 
 # FEEDING PATHS
-path_feeding="FE_tracks_original/FE_tracks_${FEEDINGTIME}/FE_tracks_${FEEDINGTIME}_$BLOCK"
-path_csv_feeding="$rootserver/$path_feeding"
-path_csv_feeding_local="$root_local/$path_feeding"
+path_csv_feeding="$rootserver/FE_tracks_original/FE_tracks_${FEEDINGTIME}/FE_tracks_${FEEDINGTIME}_$BLOCK"
+path_csv_feeding_local="$root_local/FE_tracks_original/FE_tracks_${FEEDINGTIME}/FE_tracks_${FEEDINGTIME}_$BLOCK"
 POSITION_STR_FRONT_FEEDING="FE_${FEEDINGTIME}_tracks_${BLOCK}_front"
 POSITION_STR_BACK_FEEDING="FE_${FEEDINGTIME}_tracks_${BLOCK}_back"
 
