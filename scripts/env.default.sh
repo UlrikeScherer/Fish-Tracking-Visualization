ecfdfaf820c79c8725fc493e8d2ebca66f6e46b4
Luka Stärk <luka.staerk@outlook.com>
2022-11-04 18:30:22 +0100

    update

--- scripts/env.default.sh
+++ scripts/env.default.sh
@@ -14,14 +14,18 @@ BLOCK="block1" # SPICIFY THE BLOCK TO RUN THE SCRIPT ON (block1, block2,...)
 STIME="060000" #"140000" START TIME FOR THE EXPERIMENT
 # rootserver is the path to the data on the server and the directory where the data is stored
 rootserver="/Volumes/data/loopbio_data/FE_(fingerprint_experiment)_SepDec2021"  # On MocOS the path to the root of the data
-path_recordings="$rootserver/FE_recordings/FE_recordings_$BLOCK/FE_${BLOCK}_recordings_*" # path to the recordings, will allway be on the server
+#path_recordings="$rootserver/FE_recordings/FE_recordings_$BLOCK/FE_${BLOCK}_recordings_*" # path to the recordings, will allway be on the server
+path_recordings="/Volumes/data/loopbio_data/02_SE_(superstition_experiment)_JunNov2022/SE_recordings_noisyBackup_07/SE_recordings_phaseII_maze_trials_original/SE_recodings_phaseII_maze_block1_12Jul27Jul2022"
 path_csv="$rootserver/FE_tracks"
 # --- The Path to the local data ------------
-path_csv_local="/Volumes/Extreme_SSD/FE_tracks"
-POSITION_STR_FRONT="FE_${BLOCK}_${STIME}_front_final"
-POSITION_STR_BACK="FE_${BLOCK}_${STIME}_back_final"
+path_csv_local="/Volumes/Extreme_SSD/SE_tracks_final"
+POSITION_STR_FRONT="SE_phase2_tracks_final_block1_front" #"FE_${BLOCK}_${STIME}_front_final"
+POSITION_STR_BACK="SE_phase2_tracks_final_block1_back" #"FE_${BLOCK}_${STIME}_back_final"
 dir_front=$path_csv_local/$POSITION_STR_FRONT
 dir_back=$path_csv_local/$POSITION_STR_BACK
+
+FEEDING_SHAPE="ellipse" # ellipse or rectangle
+SERVER_FEEDING_TIMES_FILE="/Volumes/data/loopbio_data/02_SE_(superstition_experiment)_JunNov2022/SE_recordings_phasell_maze_trials_times.csv"
 
 export BLOCK
 export rootserver
