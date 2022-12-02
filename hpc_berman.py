e6b8ff851091b0b37510fdffa3e5c3a70cab3df9
Luka <luka.staerk@campus.tu-berlin.de>
2022-12-02 16:14:54 +0100

    update hps

--- hpc_berman.py
+++ hpc_berman.py
@@ -84,10 +84,9 @@ def fit_data(parameters):
 
         clusters_dict = mmpy.findClusters(projections, parameters)
         for key, value in clusters_dict.items():
-            hdf5storage.write(data = value, path = '/', truncate_existing = True,
-                        filename = projectionFiles[i][:-4]+'_%s.mat'% (key), store_python_metadata = False,
-                          matlab_compatible = True)
-        del clusters_dict      
+            hdf5storage.write(data = {"clusters":value, "k":int(key.split("_")[1])}, path = '/', truncate_existing = True,
+                        filename = projectionFiles[i][:-4]+'_%s.mat'% (key), store_python_metadata = False, matlab_compatible = True)
+        #del clusters_dict      
         # Skip if embeddings already found.
         if os.path.exists(projectionFiles[i][:-4] +'_%s.mat'%(zValstr)):
             print('Already done. Skipping.\n')
