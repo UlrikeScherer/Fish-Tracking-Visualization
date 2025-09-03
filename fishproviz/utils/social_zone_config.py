093dfb58f0c9d23aa9a9b542d69185a61117b1cc
Ghadi El Hasbani <ghadielhasbani@gmail.com>
2025-09-03 14:42:36 +0300

    Update social_zone_config.py

--- fishproviz/utils/social_zone_config.py
+++ fishproviz/utils/social_zone_config.py
@@ -15,18 +15,18 @@ def read_social_zone_data_from_json():
                 maze_dict['_'.join([file.split('_')[0], "front"])] = {}
                 maze_dict['_'.join([file.split('_')[0], "back"])] = {}
             # with open(os.path.join(config.OBJECT_ZONE_COORDS_PATH, file), "r") as f:
-            with codecs.open(os.path.join(config.OBJECT_ZONE_COORDS_PATH, file), "r", encoding='utf-8',
-                             errors='ignore') as f:
-                file_data = json.load(f)
-                print(file)
-                for data in file_data:
-                    try:
+
+            try:
+                with codecs.open(os.path.join(config.OBJECT_ZONE_COORDS_PATH, file), "r", encoding='utf-8',
+                                 errors='ignore') as f:
+                    file_data = json.load(f)
+                    for data in file_data:
                         if not '_'.join(file.split('_')[1:3]).split('.')[0] in maze_dict['_'.join([file.split('_')[0], data['comment'].split('_')[0]])]:
                             maze_dict['_'.join([file.split('_')[0], data['comment'].split('_')[0]])]['_'.join(file.split('_')[1:3]).split('.')[0]] = {}
                         maze_dict['_'.join([file.split('_')[0], data['comment'].split('_')[0]])]['_'.join(file.split('_')[1:3]).split('.')[0]][data['comment'].split('_')[1]] = data
 
-                    except json.decoder.JSONDecodeError as e:
-                        print(f"Something wrong with filename {file}, raises json.decoder.JSONDecodeError")
-                        raise e
+            except json.decoder.JSONDecodeError as e:
+                print(f"Something wrong with filename {file}, raises json.decoder.JSONDecodeError")
+                raise e
     return maze_dict
 
