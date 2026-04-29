2d2c45a880f52cb71c9b3228aed1cca89223defd
Aerius01 <david.joh.james@gmail.com>
2026-04-29 11:55:38 +0200

    Corrected small closure risk

--- fishproviz/metrics/metrics.py
+++ fishproviz/metrics/metrics.py
@@ -95,10 +95,10 @@ def entropy(data, frame_interval, error_index, area):
 
 
 def distance_to_wall(data, frame_interval, error_index, area):
+    fish_key = area[0]
     avg_func = lambda chunk: mean_std(
             px2cm(distance_to_wall_chunk(chunk, area[1]), fish_key=fish_key)
         )
-    fish_key = area[0]
     return calculate_result_for_interval(
         data.astype('double'),
         frame_interval,
